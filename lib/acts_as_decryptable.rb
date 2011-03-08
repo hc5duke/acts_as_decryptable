1ea005ea5dd3c74403d388ae2d53a67f3e8f80da
hc5duke <hc5duke@gmail.com>
2011-03-08 10:36:36 -0800

    init

---
+++ lib/acts_as_decryptable.rb
@@ -0,0 +1,68 @@
+module ActiveRecord
+  module Acts
+    module Decryptable
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      # class SomeModel < ActiveRecord::Base
+      #   acts_as_decryptable :encrypt => [:name, :email], :key => "SOME_KEY", :show => '*' * 8
+      # end
+      #
+      # some_model_object.name = "name"
+      # some_model_object.save
+      # some_model_object.name                        # => "encrypted name" or "********"
+      # some_model_object.decrypted_name              # => "name"
+      module ClassMethods
+        def acts_as_decryptable(options)
+          cattr_accessor :encrypted_columns
+          configuration = { :encrypt => [] }
+          configuration.update(options) if options.is_a?(Hash)
+          raise "Valid key must be specified" unless configuration[:key]
+
+          configuration[:encrypt].each do |field_name|
+            class_eval <<-EOV
+              def #{field_name}=(value)
+                if value.present? && value != #{configuration[:show].to_json}
+                  self[:#{field_name}] = SymmetricCrypto.encrypt(value, #{configuration[:key].to_json})
+                end
+              end
+
+              def decrypt_#{field_name}
+                SymmetricCrypto.decrypt(self[:#{field_name}], #{configuration[:key].to_json})
+              end
+            EOV
+
+            if configuration[:show]
+              class_eval <<-EOV
+                def #{field_name}
+                  self[:#{field_name}].nil? ? nil : #{configuration[:show].to_json}
+                end
+              EOV
+            end
+          end
+        end
+      end
+
+      class SymmetricCrypto
+        def self.encrypt(text, key)
+          aes(:encrypt, text, key)
+        end
+
+        def self.decrypt(crypted, key)
+          aes(:decrypt, crypted, key)
+        end
+
+      private
+        def self.aes(direction, message, key)
+          cipher = OpenSSL::Cipher.new('AES256')
+          direction == :encrypt ? cipher.encrypt : cipher.decrypt
+          cipher.key = key
+          cipher.update message
+          cipher.final
+        end
+      end
+
+    end
+  end
+end
---
+++ test/acts_as_decryptable_test.rb
@@ -0,0 +1,65 @@
+require 'test/unit'
+
+require 'rubygems'
+gem 'activerecord', '>= 1.15.4.7794'
+require 'active_record'
+
+require "#{File.dirname(__FILE__)}/../init"
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+def setup_db
+
+  ActiveRecord::Schema.define(:version => 1) do
+    create_table :mixins do |t|
+      t.column :plaintext, :string
+      t.column :encrypted, :text
+      t.column :created_at, :datetime
+      t.column :updated_at, :datetime
+    end
+  end
+end
+
+def teardown_db
+  ActiveRecord::Base.connection.tables.each do |table|
+    ActiveRecord::Base.connection.drop_table(table)
+  end
+end
+
+class Mixin < ActiveRecord::Base
+end
+
+class DecryptableMixin < Mixin
+  acts_as_decryptable :encrypt => [ :encrypted ], :key => 'a' * 32, :show => '*' * 8
+
+  def self.table_name() "mixins" end
+end
+
+class DecryptTest < Test::Unit::TestCase
+
+  def setup
+    setup_db
+    DecryptableMixin.create!
+  end
+
+  def teardown
+    teardown_db
+  end
+
+  def test_encrypt
+    decryptable = DecryptableMixin.first
+    decryptable.plaintext = "text"
+    decryptable.encrypted = "text"
+    decryptable.save!
+
+    assert_equal "text",  decryptable.plaintext
+    assert_equal "text",  decryptable.decrypt_encrypted
+    assert_equal "*" * 8, decryptable.encrypted
+
+    decryptable.encrypted = "*" * 8
+    decryptable.save
+    assert_equal "text",  decryptable.decrypt_encrypted
+    assert_equal "*" * 8, decryptable.encrypted
+  end
+end
+
